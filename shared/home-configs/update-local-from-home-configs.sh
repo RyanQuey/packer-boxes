c076c39b2fe2ae442b7bb3533dbfbb9e0ec6fa7c
Ryan Quey <quey.ryan@gmail.com>
2020-05-21 07:32:37 +0000

    use var for home for shell scripts

--- shared/home-configs/update-local-from-home-configs.sh
+++ shared/home-configs/update-local-from-home-configs.sh
@@ -3,5 +3,5 @@
 # copies all the local home config files from this repository to this local environment
 # NOTE not to be ran within the packer build process, but to be ran while developing this packer project, IN THE VM not in the virtualbox host
 
-cp -r /home/vagrant/projects/packer-boxes/shared/home-configs/{.vimrc,.tmux.conf,.bashrc,.inputrc,.gitconfig,.gitignore,.vim/} ~/ 
+cp -r $HOME/projects/packer-boxes/shared/home-configs/{.vimrc,.tmux.conf,.bashrc,.inputrc,.gitconfig,.gitignore,.vim/} ~/ 
 exec $SHELL
