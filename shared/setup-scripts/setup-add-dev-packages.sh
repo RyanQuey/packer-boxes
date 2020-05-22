9b8ca158ede5455abd737442ecef4a31d3bd895f
Ryan Quey <quey.ryan@gmail.com>
2020-05-22 05:53:46 +0000

    more box setup scripts

--- shared/setup-scripts/setup-add-dev-packages.sh
+++ shared/setup-scripts/setup-add-dev-packages.sh
@@ -1,2 +1,3 @@
+# this file should be changed fairly often, depending on what projects I have been working on recently. But getting docker is pretty standard.
 sudo bash ~/projects/packer-boxes/easy-boot-ubuntu1804/scripts/initialize.sh
 sudo bash ~/projects/packer-boxes/easy-boot-ubuntu1804/scripts/docker.sh
