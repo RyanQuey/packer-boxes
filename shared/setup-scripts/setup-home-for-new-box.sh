d248a7ad664f1e64532bc9061e4f79940482e99e
Ryan Quey <rlquey2@gmail.com>
2020-10-19 15:05:12 -0700

    Update setup-home-for-new-box.sh

--- shared/setup-scripts/setup-home-for-new-box.sh
+++ shared/setup-scripts/setup-home-for-new-box.sh
@@ -1,5 +1,6 @@
 # make sure to call with `sudo` 
 # combines several of the other scripts into one to run
+# download and run using: sudo apt-get install git && curl -LO https://raw.githubusercontent.com/RyanQuey/packer-boxes/master/shared/setup-scripts/setup-home-for-new-box.sh && sudo bash setup-home-for-new-box.sh
 # TODO add some options depending on what I want to setup for this box, and add cli flags to call those options
 
 mkdir -p $HOME/projects
