59321cd1c035c2ecefeb4f0d4b2288cbba1ee606
Ryan Quey <quey.ryan@gmail.com>
2021-03-31 21:59:25 +0700

    fix ycm

--- easy-boot-ubuntu1804/scripts/_you_complete_me.sh
+++ easy-boot-ubuntu1804/scripts/_you_complete_me.sh
@@ -20,6 +20,7 @@ sudo apt-get install g++-8
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 700 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
 
+
 ## https://stackoverflow.com/a/40131786/6952495
 ### assumes we have 3.6 installed, which I think we do automatically above with python3-dev (?)
 ### NOTE YCM recommends at least vim 8.1 for java completion, and default as of May 2020 is 8.0
@@ -27,5 +28,9 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /
 ### Maybe don't want --clang-completer, it's for C family and I don't use C, C# etc...but instructions say to use it in some places
 ### --all only works if we have go and mono-complete installed already. Not doing for now
 # /usr/bin/python3.6 ./install.py --all
-sudo /usr/bin/python3.6 ./install.py  --java-completer --ts-completer
+
+# don'tuse sudo, since can't use npm without sudo without further setup. 
+# therefore, need to change some permissions first
+sudo chown -R $USER . && \
+/usr/bin/python3.6 ./install.py  --java-completer --ts-completer
 
