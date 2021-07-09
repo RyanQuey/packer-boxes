a8270834220a022ffa9674fff03bf5c0840cbbec
Ryan Quey <quey.ryan@gmail.com>
2021-07-09 14:49:11 +0700

    also include some ssh files and intellij killer

--- shared/setup-scripts/_shared_vars.sh
+++ shared/setup-scripts/_shared_vars.sh
@@ -1,2 +1,2 @@
 
-export HOME_CONFIGS_TO_SYNC_WITH_PACKER_REPO=.vimrc,.tmux.conf,.bash_profile,.bashrc,.inputrc,.gitconfig,.gitignore,.vim/,global_extra_conf.py,start-intellij.sh,.bash_completion,.bash_completion.d
+export HOME_CONFIGS_TO_SYNC_WITH_PACKER_REPO=.vimrc,.tmux.conf,.bash_profile,.bashrc,.inputrc,.gitconfig,.gitignore,.vim/,global_extra_conf.py,start-intellij.sh,kill-intellij.sh,.bash_completion,.bash_completion.d,ssh.optiplex.sh,global_extra_conf.py
